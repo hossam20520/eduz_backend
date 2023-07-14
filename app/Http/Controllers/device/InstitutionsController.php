ba3608fe6752f6b2b443badd7d6d858f13ef0ad4
hossam hassan <hossamhassan889@gmail.com>
2023-07-15 00:42:25 +0300

    select

--- app/Http/Controllers/device/InstitutionsController.php
+++ app/Http/Controllers/device/InstitutionsController.php
@@ -15,7 +15,7 @@ class InstitutionsController extends Controller
     public function GetAllEducation(Request $request ){
 
 
-      $education =  Education::where('deleted_at', '=', null )->get();
+      $education =  Education::where('deleted_at', '=', null )->select('id', 'ar_name' , 'en_name' , 'image' , 'lat' , 'long_a' )->get();
 
       return response()->json([
         'educations' =>  $education,
