531d05541d5f6d953bd05776dd45cce5668c57d1
hossam hassan <hossamhassan889@gmail.com>
2023-07-15 03:32:08 +0300

    adDDf

--- app/Http/Controllers/EducationsController.php
+++ app/Http/Controllers/EducationsController.php
@@ -247,7 +247,7 @@ class EducationsController extends BaseController
                     }
                     $files = $request['images'];
                     foreach ($files as $file) {
-                        $fileData = ImageResize::createFromString(base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $file['path'])));
+                        $fileData =  base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $file['path']));
                         // $fileData->resize(200, 200);
                         $name = rand(11111111, 99999999) . $file['name'];
                         $path = public_path() . '/images/educations/';
