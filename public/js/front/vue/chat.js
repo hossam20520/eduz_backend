f250fac3aa237bc648b94748b5e2dd0e0fbad5ab
hossam hassan <hossamhassan889@gmail.com>
2023-07-22 23:22:41 +0300

    hello

--- public/js/front/vue/chat.js
+++ public/js/front/vue/chat.js
@@ -126,7 +126,7 @@ new Vue({
           db.collection("users_messages")
           .doc(this.receiver_id)
           .collection("messages")
-          .where("to", "==", this.receiver_id)
+          .where("to", "==", "2")
           .get()
           .then((querySnapshot) => {
             // Loop through the query results (there should be only one result if "to" is unique)
