bef3294612fc4285a3ae48d904be0d7496ee2a25
hossam hassan <hossamhassan889@gmail.com>
2023-09-24 14:21:16 +0300

    fiux

--- routes/api.php
+++ routes/api.php
@@ -72,9 +72,9 @@ Route::post("device/users/message/", "device\AuthController@GetUsers");
 Route::middleware(['auth:api', 'Is_Active'])->group(function () {
 
 
-    Route::get('device/cart/delete', 'device\ProductsController@deleteCart');
+    Route::get('device/cart/delete', 'device\ProductController@deleteCart');
 
-
+    
 
     Route::get('device/product', 'device\ProductController@Get_Products_Details');
 
