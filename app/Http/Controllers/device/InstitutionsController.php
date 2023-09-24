dc100a9a93542baf32c8618cc1fa7b23cfef4547
hossam hassan <hossamhassan889@gmail.com>
2023-09-24 11:18:48 +0300

    eerdddF

--- app/Http/Controllers/device/InstitutionsController.php
+++ app/Http/Controllers/device/InstitutionsController.php
@@ -183,7 +183,7 @@ class InstitutionsController extends Controller
 
 // Convert the string of IDs into an array
             $idsArray = explode(', ', $ids);
-
+           return $idsArray;
             $results = $model::where(function($query) use ($idsArray) {
               foreach ($idsArray as $id) {
                   $query->orWhereRaw('FIND_IN_SET(?, selected_ids) > 0', [$id]);
