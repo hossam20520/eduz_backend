44a486a0959347ed5db1e2df88b5f3157e5a8a84
hossam hassan <hossamhassan889@gmail.com>
2023-11-06 22:32:44 +0200

    drreqqq

--- app/Http/Controllers/device/InstitutionsController.php
+++ app/Http/Controllers/device/InstitutionsController.php
@@ -237,7 +237,7 @@ class InstitutionsController extends Controller
                   $query->orWhereRaw('FIND_IN_SET(?, selected_ids) > 0', [$id]);
                 }else if($section->en_name ==   "Government"){
                   $query->where('selected_ids', 'LIKE', '['.$id.',%');
-                 
+                  $query->orWhere('selected_ids', 'LIKE', '%,'.$id.',%');
                   $query->orWhere('selected_ids', 'LIKE', ''.$id.',%');
                   $query->orWhere('selected_ids', 'LIKE', '%,'.$id.']');
                   $query->orWhere('selected_ids', '=',  $id );
